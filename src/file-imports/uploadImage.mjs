361e9b75b764c6e6cb204705aec4e786d60fd658
Philipp Staender <pstaender@mailbox.org>
2025-08-15 05:23:00 +0200

    implements s3 image upload and showing images

---
+++ src/file-imports/uploadImage.mjs
@@ -0,0 +1,25 @@
+import * as s3 from "../s3";
+import { sha1 } from "../helper";
+
+/**
+ *
+ * @param {DataTransferItem} transferItem
+ * @param {File} file
+ */
+export function uploadImage(transferItem, filename, fileExtension, onFinishedCallback) {
+  const f = transferItem.getAsFile();
+  const reader = new FileReader();
+  reader.onload = async (event) => {
+    const arrayBuffer = event.target.result;
+    if (fileExtension) {
+      let hash = await sha1(arrayBuffer);
+      hash = hash.substring(0, 8); // shorten to 8 characters
+      filename = `images/${hash}.${fileExtension}`;
+    } else {
+      filename = `images/${filename}`;
+    }
+    await s3.uploadBinaryFile(filename, arrayBuffer, transferItem.type);
+    onFinishedCallback({ filename });
+  };
+  reader.readAsArrayBuffer(f);
+}
